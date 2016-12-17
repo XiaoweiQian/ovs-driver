ab6b7165954dec398aa9918fdcddb021dd72fc15
xiaowei <xiaowei@xiaoweideMacBook-Pro.local>
2016-12-17 11:31:59 +0800

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/XiaoweiQian/ovs-driver/ovs"
+)
+
+func main() {
+
+	d := ovs.Driver{}
+	fmt.Println(1122333)
+	fmt.Println(d)
+	//h := network.NewHandler(d)
+	//h.ServeUnix("root", "mydriver")
+}
